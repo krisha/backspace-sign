cd83d280dffb9854476af9f2254454b60481172d
krisha <krisha@chrisi.de>
2013-10-08 22:02:15 +0200

    sine application added

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ This repository is for the software stuff of our backspace sign.
 
 To light our sign we bought 3m of the addressable RGB stripes from insomnia lightning. Those stripes have a LPD8086 controller IC for every 2 RGB LEDs.
 
-The server is running on a Raspberry Pi on the SPI port and takes complete frames from UDP on port 10001. Each RGB LED takes 3 bytes. Each byte must have the MSB bit set (logical OR with 0x80). So the brightness can be regulated in upto 127 steps.
+The server is running on a Raspberry Pi, takes a complete frames from UDP on port 10001 and sends it to SPI. Each RGB LED takes 3 bytes. Each byte must have the MSB bit set (logical OR with 0x80). So the brightness can be regulated in upto 127 steps.
 
 Sample command line to set all LEDs to white:
 perl -e 'print "\xFF\xFF\xFF"x156' | nc -u <IP> 10001
---
+++ clients/sine.cs
@@ -0,0 +1,118 @@
+﻿/* 
+ * draws a rotating sine around our sign
+ * use keys cursor_left/right, R, G, B, D, C to control
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+
+namespace SomethingForLEDSign
+{
+    class Program
+    {
+        static byte brightness_sine(int x, int offset, int period)
+        {
+            double s = Math.Sin(2 * Math.PI / period * x + (double)2 * Math.PI * offset / period) * 127.0;
+
+            s = Math.Abs(s);
+            return (byte)s;
+        }
+
+        enum RGB
+        {
+            Red,
+            Green,
+            Blue
+        };
+
+        static void Main(string[] args)
+        {
+            const int LED_COUNT = 156;
+            const int LEDS_USED = 131;
+
+            byte[] data = new byte[LED_COUNT * 3];
+            for (int i = 0; i < LED_COUNT * 3; i++)
+                data[i] = 0x80;
+
+            UdpClient client = new UdpClient();
+
+            bool direction = true;
+            int speed = 10;
+            int X = 40;
+            RGB rgb = RGB.Blue;
+
+            while (true)
+            {
+
+                if (Console.KeyAvailable)
+                {
+
+                    switch (Console.ReadKey(true).Key)
+                    {
+                        case ConsoleKey.LeftArrow:
+                            speed /= 2;
+                            if (speed < 1)
+                                speed = 1;
+                            break;
+                        case ConsoleKey.RightArrow:
+                            speed *= 2;
+                            if (speed > 500)
+                                speed = 500;
+                            break;
+                        case ConsoleKey.C:
+                            return;
+                        case ConsoleKey.D:
+                            direction ^= true;
+                            break;
+                        case ConsoleKey.R:
+                            rgb = RGB.Red;
+                            break;
+                        case ConsoleKey.G:
+                            rgb = RGB.Green;
+                            break;
+                        case ConsoleKey.B:
+                            rgb = RGB.Blue;
+                            break;
+
+                    }
+                    Console.WriteLine("speed: " + speed);
+
+                    while (Console.KeyAvailable)
+                        Console.ReadKey(true);
+
+                }
+
+                for (int i = 0; i < LEDS_USED; i++)
+                {
+                    byte brightness = brightness_sine(i, X, LEDS_USED);
+
+                    data[i * 3 + 0] = (byte)(0x80 | (rgb == RGB.Blue ? brightness : 0));
+                    data[i * 3 + 1] = (byte)(0x80 | (rgb == RGB.Red ? brightness : 0));
+                    data[i * 3 + 2] = (byte)(0x80 | (rgb == RGB.Green ? brightness : 0));
+                }
+
+                if (direction)
+                {
+                    X++;
+                    X %= LEDS_USED;
+                }
+                else
+                {
+                    X--;
+                    if (X == -1)
+                        X = LEDS_USED - 1;
+                }
+
+
+
+                client.Send(data, data.Length, "schild", 10001);
+
+                System.Threading.Thread.Sleep(10 + speed);
+            }
+        }
+    }
+}
